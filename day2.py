7b04ab2d5246810a1b5563759baadd934f6e6f02
JoannaKwiatkowska <joarie@wp.pl>
2019-10-22 22:02:57 +0200

    dodalam cwiczenia z zajec

--- day2.py
+++ day2.py
@@ -1,1 +1,31 @@
-print(2+2)
+#x = input("Podaj liczbę pierwszą: ")
+#y = input("Podaj liczbę druga: ")
+
+#print("1"+"4")
+#suma= int(x) + int(y)
+
+#print(suma)
+
+# exit()
+# comments
+"""comments 
+in few lines
+"""
+
+#tekst = "Tytuł: \"Ala ma kota\""
+#tekst = """Tytuł: "Ala ma kota" """
+tekst = 'Tytuł: "Ala ma kota"'
+#tekst="test\test\nowy test"
+#tekst=r"test\test\nowy test"
+#tekst="wiele\nlinijek\ntekstu"
+#print(tekst)
+#print(tekst.upper())
+dlugosc = len(tekst)
+print(tekst[0])
+print(tekst[3:5])
+print(tekst[dlugosc-1])
+#ulamek = 1.34
+#ilosc_powtorzen = 4
+
+#print(ilosc_powtorzen*tekst)
+#print(10*"=====")
