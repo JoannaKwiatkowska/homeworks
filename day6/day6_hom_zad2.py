24dbc80330dceee2a467c4a1a29f79f2566765b5
JoannaKwiatkowska <joarie@wp.pl>
2019-11-10 01:29:49 +0100

    dzien 6 zadania domowe - zadanie 1

---
+++ day6/day6_hom_zad2.py
@@ -0,0 +1,9 @@
+# 2) Napisz/dokończ program z zajęć "Księga gości". Program oparty o dowolny format pliku (csv, pickle, własny).
+#   Program powinien mieć mini menu z odtępnymi funkcjami::
+#   A) dodawanie nowego wpisu
+#   B) wyszukiwanie wpisów po treści
+#   C) wyświetlanie wszystkich wpisów od najstarszych
+#   D) wyświetlanie wszystkich wpisów od najmłodszych
+#   E) wyświetlanie pojedynczych wpisów, N - następny, P - poprzedni
+#   F) wyświetlanie losowowego wpisu
+#   X) wyjście z programu
