9bebb4bf1aa40dfc4488657838db4b0d578c17a1
JoannaKwiatkowska <joarie@wp.pl>
2019-11-17 00:27:05 +0100

    funkcja do zadania domowego z ćwiczeń 8

---
+++ day8/kalkulator_F_C.py
@@ -0,0 +1,4 @@
+def kalkulator_kelwin_celsjusz(kelwiny):
+    """Kalkulator do przeliczania stopni Fahrenheita na Celsjusza"""
+    celsjusz_temp = round((kelwiny - 273.15), 1)
+    return celsjusz_temp
